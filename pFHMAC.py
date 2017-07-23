05ff96c4a2c76374874d0ca2596e8aa0af4c96a1
liusongee <liusong_ee@163.com>
2017-07-23 23:45:26 +0800

    global parameter setting

--- pFHMAC.py
+++ pFHMAC.py
@@ -1,2 +1,6 @@
 # this is a simulator for pFHMAC, polling based full-/half-duplex MAC protocol
 import numpy as np
+
+################################################################################
+# global parameter settting
+################################################################################
